714c4111aaa9f04ada776ba025707b3fd5decfc0
RRR777111 <72430802+RRR777111@users.noreply.github.com>
2021-02-20 21:41:14 +0900

    lightbox風モーダル追加

--- main.js
+++ main.js
@@ -11,4 +11,25 @@
             picImg.src = attr;
         });
     });
+
+
+    // $(function(){
+        $('#mv__img').on('click', function(){
+            $('#wrapper').append('<div class="bg" id="bg">');
+            $('#bg').hide();        
+            $('#bg').append('<div class="photo" id="photo" alt="photo">');
+            $('#photo').append('<img class="photo__img" id="photo__img">');
+            $('#photo__img').attr('src', $(this).attr('src'));
+            $('#bg').fadeIn();
+            
+            $('#bg').on('click', function(){
+                $(this).fadeOut(function(){
+                    $(this).remove();
+                });
+            });
+            return false;
+        });
+    // });
+
+
 }
